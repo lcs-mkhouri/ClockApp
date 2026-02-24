9755faa8b4428350bf81f87d1b22693e92968c66
MIchel Khouri <mkhourisanchez@lcs.on.ca>
2026-02-24 15:24:50 -0500

    World clock done.

---
+++ ClockApp/Views/CityView.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct CityView: View {
+    
+    let timeZoneOffset: String
+    let city: String
+    let time: String
+    let amOrPm: String
+    
+    
+    
+    
+    var body: some View {
+        HStack{
+            VStack{
+                Text(
+                    "Today, \(timeZoneOffset)HRS"
+                )
+                Text(
+                    city
+                )
+                .font(
+                    .system(
+                        .largeTitle,
+                        design: .default,
+                        weight: .thin
+                    )
+                )
+            }
+            Spacer()
+            Text(
+                time
+            )
+            .font(
+                .system(
+                    size: 64.0,
+                    weight: .thin,
+                    design: .default
+                )
+            )
+            Text(
+                amOrPm
+            )
+            .font(
+                .system(
+                    .largeTitle,
+                    design: .default,
+                    weight: .thin
+                )
+            )
+        }
+    }
+}

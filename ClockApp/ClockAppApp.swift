5bb30fcfb4a29ee751f80ea7ddadac7898800629
MIchel Khouri <mkhourisanchez@lcs.on.ca>
2026-02-24 14:15:19 -0500

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Michel Khouri on 2026-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+

3dc42f5e60285a1d57adb5e5ca88039bdab77acc
MIchel Khouri <mkhourisanchez@lcs.on.ca>
2026-02-24 15:25:45 -0500

    Adding support for multiple cities.

--- ClockApp/Views/CityView.swift
+++ ClockApp/Views/CityView.swift
@@ -1,3 +1,11 @@
+//
+//  CityView.swift
+//  ClockApp
+//
+//  Created by Michel Khouri on 2026-02-24.
+//
+
+
 import SwiftUI
 
 struct CityView: View {
--- ClockApp/Views/WorldClockView.swift
+++ ClockApp/Views/WorldClockView.swift
@@ -10,29 +10,68 @@ import SwiftUI
 struct WorldClockView: View {
     var body: some View {
         NavigationStack {
-            Text("World Clock")
-                .navigationTitle("World Clock")
+            VStack{
+                CityView(
+                    timeZoneOffset: "+0",
+                    city: "Ottawa",
+                    time: "6:35",
+                    amOrPm: "AM"
+                )
+                CityView(
+                    timeZoneOffset: "-3",
+                    city: "Vancouver",
+                    time: "3:35",
+                    amOrPm: "AM"
+                )
+                CityView(
+                    timeZoneOffset: "+6",
+                    city: "Madrid",
+                    time: "12:35",
+                    amOrPm: "PM"
+                )
+                CityView(
+                    timeZoneOffset: "-1",
+                    city: "CDMX",
+                    time: "5:35",
+                    amOrPm: "AM"
+                )
+                
+                
+                
+                
+                .navigationTitle(
+                    "World Clock"
+                )
                 .toolbar {
                     
-                    ToolbarItem(placement: .topBarLeading) {
+                    ToolbarItem(
+                        placement: .topBarLeading
+                    ) {
                         
-                        Button("Edit") {
+                        Button(
+                            "Edit"
+                        ) {
                             // Does nothing right now
                         }
                         
                     }
                     
-                    ToolbarItem(placement: .primaryAction) {
+                    ToolbarItem(
+                        placement: .primaryAction
+                    ) {
                         Button{
                         }label: {
-                            Image(systemName: "plus")
-                        }
+                            Image(
+                                systemName: "plus"
+                            )
                         }
                     }
                 }
+            }
         }
     }
-
+}
 #Preview {
     LandingView()
 }
+
